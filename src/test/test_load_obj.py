d397829abf6b29a67a8a722068a12b5665ceb1b5
ChamRun <chrm@aut.ac.ir>
2023-01-28 16:00:02 +0330

    Create test_load_obj.py

---
+++ src/test/test_load_obj.py
@@ -0,0 +1,28 @@
+import unittest
+from src import save_obj, load_obj
+
+
+class TestLoadObj(unittest.TestCase):
+    def test_load_obj(self):
+        obj = {'name': 'John', 'age': 30}
+        name = 'test_obj'
+        path = './test_rem/'
+        save_obj(obj, name, path)
+        loaded_obj = load_obj(name, path)
+        self.assertEqual(obj, loaded_obj)
+
+    def test_load_obj_with_default_path(self):
+        obj = {'name': 'John', 'age': 30}
+        name = 'test_obj'
+        save_obj(obj, name)
+        loaded_obj = load_obj(name)
+        self.assertEqual(obj, loaded_obj)
+
+    def test_load_obj_with_non_existent_file(self):
+        name = 'non_existent_file'
+        loaded_obj = load_obj(name)
+        self.assertIsNone(loaded_obj)
+
+
+if __name__ == '__main__':
+    unittest.main()
