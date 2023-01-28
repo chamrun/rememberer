ee63604047923356f658c11b6a0c296a93bfcf11
ChamRun <chrm@aut.ac.ir>
2023-01-28 17:22:39 +0330

    add TestRem

---
+++ src/test/test_rem.py
@@ -0,0 +1,34 @@
+import unittest
+import time
+
+from src import rem
+
+
+def test_func(a, b):
+    time.sleep(3)
+    return a + b
+
+
+class TestRem(unittest.TestCase):
+    def test_rem(self):
+        result1 = rem(test_func, 1, 2)
+
+        start = time.time()
+        result2 = rem(test_func, 1, 2)
+        end = time.time()
+
+        self.assertLess(end - start, 1)
+        self.assertEqual(result1, result2)
+
+    def test_rem_with_kwargs(self):
+        result = rem(test_func, 1, b=2)
+        self.assertEqual(result, 3)
+
+    def test_rem_with_cached_result(self):
+        rem(test_func, 1, 2)
+        result = rem(test_func, 1, 2)
+        self.assertEqual(result, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
