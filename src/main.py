425dac03feb47d5daf1db6168bb06d1d71d764b1
ChamRun <chrm@aut.ac.ir>
2023-01-28 14:40:21 +0330

    add `save_obj` function

--- src/main.py
+++ src/main.py
@@ -1,2 +1,31 @@
-def rem():
-    pass
+import os
+import pickle
+import hashlib
+
+
+def save_obj(obj, name=None, path='./pickles/'):
+    if path[-1] != '/':
+        path += '/'
+
+    if not name:
+        hash_object = hashlib.sha256()
+        hash_object.update(pickle.dumps(obj))
+        name = hash_object.hexdigest()
+
+    current_dir = os.getcwd()
+    for folder_name in path.split('/'):
+        if not folder_name:
+            continue
+
+        if not os.path.exists(folder_name):
+            os.mkdir(folder_name)
+
+        os.chdir(folder_name)
+
+    with open(f'{name}.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+        abspath = os.path.abspath(f.name)
+
+    os.chdir(current_dir)
+    return abspath
+
